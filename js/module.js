f3b0fe4aeb0246c5702a1a84fb5745682a606c3c
Simon Garnotel <simon.garnotel@gmail.com>
2018-11-09 15:59:51 +0100

    Update module menu button

--- js/module.js
+++ js/module.js
@@ -1,6 +1,14 @@
 showNav = () => {
-  if (nav.style.display === 'block')
+  if (nav.style.display === 'block') {
     nav.style.display = 'none'
-  else
+    if (navImage.src.includes('menu-close.svg')) {
+      navImage.src = navImage.src.replace('menu-close.svg', 'menu.svg')
+    }
+  }
+  else {
     nav.style.display = 'block'
+    if (navImage.src.includes('menu.svg')) {
+      navImage.src = navImage.src.replace('menu.svg', 'menu-close.svg')
+    }
+  }
 }
