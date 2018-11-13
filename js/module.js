fe21210a3b9f632c0d21a83552750e6b34f5390b
franckl <franck.lahaye@gmail.com>
2018-11-13 13:26:01 +0100

    Update js/module.js

--- js/module.js
+++ js/module.js
@@ -1,13 +1,13 @@
 showNav = () => {
   if (nav.style.display === 'block') {
     nav.style.display = 'none'
-    content.style.display = 'block'
+    document.getElementById('content').style.display = 'block'
     navImage.style.display = 'block'
     navImageClose.style.display = 'none'
   }
   else {
     nav.style.display = 'block'
-    content.style.display = 'none'
+    document.getElementById('content').style.display = 'none'
     navImage.style.display = 'none'
     navImageClose.style.display = 'block'
   }
