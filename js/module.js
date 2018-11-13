07722db248ee49f0cd82dfc6b2dd71c64417e341
Simon Garnotel <simon.garnotel@gmail.com>
2018-11-13 13:24:21 +0100

    update media menu

--- js/module.js
+++ js/module.js
@@ -1,11 +1,13 @@
 showNav = () => {
   if (nav.style.display === 'block') {
     nav.style.display = 'none'
+    content.style.display = 'block'
     navImage.style.display = 'block'
     navImageClose.style.display = 'none'
   }
   else {
     nav.style.display = 'block'
+    content.style.display = 'none'
     navImage.style.display = 'none'
     navImageClose.style.display = 'block'
   }
