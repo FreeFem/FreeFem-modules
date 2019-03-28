e5d565768ebfbb71cf3351493bf0b8557f1c49e4
franckl <franck.lahaye@gmail.com>
2019-03-28 17:57:11 +0100

    updating donate button css

--- js/donate.js
+++ js/donate.js
@@ -1,21 +1,19 @@
 const donateLink = document.getElementById('donate')
-donateLink.position = 'relative'
-
-const donateMessage = document.createElement('div')
-donateMessage.innerHTML = '<p>Coming soon</p>'
-donateMessage.style.display = 'none'
-donateMessage.style.backgroundColor = 'white'
-donateMessage.style.color = 'black'
-donateMessage.style.padding = '1rem'
-donateMessage.style.position = 'absolute'
-donateMessage.style.top = 0
-donateMessage.style.right = 0
-
-donateLink.appendChild(donateMessage)
+const nav = donateLink.parentElement.querySelectorAll('nav > a')
 
 donateLink.onclick = function() {
-	donateMessage.style.display = 'block'
-	setTimeout(function() {
-		donateMessage.style.display = 'none'
-	}, 3000)
+  donateLink.innerHTML = 'COMING SOON'
+  if (!window.matchMedia("(max-width: 1200px)").matches) {
+    donateLink.parentElement.querySelectorAll('nav > a').forEach(el => {
+      el.style.marginLeft = '2rem'
+    })
+  }
+  setTimeout(function() {
+    donateLink.innerHTML = 'DONATE'
+    if (!window.matchMedia("(max-width: 1200px)").matches) {
+      donateLink.parentElement.querySelectorAll('nav > a').forEach(el => {
+        el.style.marginLeft = ''
+      })
+    }
+  }, 3000)
 }
